01b5976777f2aa7ceb4303e502b266192ee0bcd9
pedrohsantossx <pedrohsarcanjoo@gmail.com>
2026-04-10 15:37:00 -0300

    Netlify Fix: Injetando node_env development no Toml para baixar Vite corretamente

--- enviar_pro_github.bat
+++ enviar_pro_github.bat
@@ -1,12 +1,12 @@
 @echo off
 echo =======================================
-echo.   ENVIANDO FIX DO NETLIFY...         
+echo.   ENVIANDO NOVO FIX DO NETLIFY...         
 echo =======================================
 
 git add .
-git commit -m "Netlify: Removendo PNPM config e forcando Vite nativo"
+git commit -m "Netlify Fix: Injetando node_env development no Toml para baixar Vite corretamente"
 git push -u origin main --force
 
 echo.
-echo Processo finalizado! O codigo subiu.
+echo Processo finalizado!
 pause
