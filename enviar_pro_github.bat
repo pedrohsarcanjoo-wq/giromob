5067c0c95936a15e3515d4bb0f279b81f05c0024
pedrohsantossx <pedrohsarcanjoo@gmail.com>
2026-04-10 15:35:17 -0300

    Netlify: Removendo PNPM config e forcando Vite nativo

--- enviar_pro_github.bat
+++ enviar_pro_github.bat
@@ -1,26 +1,12 @@
 @echo off
-echo.
 echo =======================================
-echo.   AMARRANDO O PROJETO COM O GITHUB!     
+echo.   ENVIANDO FIX DO NETLIFY...         
 echo =======================================
-echo.
 
-git init
-git remote add origin https://github.com/pedrohsarcanjoo-wq/giromob.git
-git remote set-url origin https://github.com/pedrohsarcanjoo-wq/giromob.git
-git branch -M main
-
-echo.
-echo Salvando e Empacotando arquivos do projeto...
 git add .
-git commit -m "Arquitetura Giromob: Dashboard Frontend Premium e Isolamento MultiUsuario Backend"
-
-echo.
-echo Enviando codigos diretos para a Web (Repositório Principal)...
+git commit -m "Netlify: Removendo PNPM config e forcando Vite nativo"
 git push -u origin main --force
 
 echo.
-echo =======================================
-echo Sucesso! Leia a mensagem acima. Se aparecer "branch 'main' set up to track", seu codigo esta na nuvem!
-echo =======================================
+echo Processo finalizado! O codigo subiu.
 pause
